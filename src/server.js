d8461d754c3440cf9e4ca12dff4b8d81df1376c6
Thibaud Colas <thibaudcolas@gmail.com>
2015-04-11 00:41:23 +1200

    Add Handlebars templates

--- src/server.js
+++ src/server.js
@@ -1,13 +1,29 @@
 var express = require('express');
+var hbs = require('hbs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+app.get('/', function(req, res) {
+
+    res.locals = {
+        test: 'Hi team!',
+        list: ['cat', 'dog']
+    }
+
+    res.render('index');
+});
+
+app.get('/hello', function(request, response) {
+    response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+    console.log('Node app is running at localhost:' + app.get('port'));
 });
