b9ca6416d9870c8ef6bdb621211c0746df2dc468
Taylor Lodge <ubermouse894@gmail.com>
2015-04-11 21:13:12 +1200

    Move house price to same line as label text

--- src/assets/scripts/pages/filterdSuburb.jsx
+++ src/assets/scripts/pages/filterdSuburb.jsx
@@ -7,8 +7,7 @@ export default React.createClass({
     return (
       <div>
         <h1 style={{textAlign: 'center'}}><b>{this.props.pageData.suburb.toUpperCase()}</b></h1>
-        <h2>Average House Price for {this.props.pageData.bedrooms} bedrooms</h2>
-        <p>${Math.floor(Math.random() * 799) + 200}</p>
+        <h2>Average House Price for {this.props.pageData.bedrooms} bedrooms: ${Math.floor(Math.random() * 799) + 200}</h2>
         <SelectRooms transitionCb={this.props.transitionCb} suburb={this.props.pageData.suburb} />
         <h2>Check another suburb</h2>
         <SuburbPicker transitionCb={this.props.transitionCb} />
