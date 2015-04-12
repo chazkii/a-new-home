ee91942e94596da432ff60f3d02c2aaf894240e4
Taylor Lodge <ubermouse894@gmail.com>
2015-04-12 14:39:08 +1200

    Add renting/buying radio buttons on frontpage

--- src/assets/scripts/pages/index.jsx
+++ src/assets/scripts/pages/index.jsx
@@ -7,6 +7,28 @@ export default React.createClass({
   render: function() {
     return (
       <div>
+        <div style={{width: '33%', margin: 'auto'}}>
+          <h1>I'm interested in...</h1>
+          <div>
+            <div style={{float: 'left', width: '50%'}}>
+              <div className="radio radio-primary">
+                <label>
+                  <input type='radio' checked/>
+                  <p style={{marginLeft: '-7px', marginTop: '-4px'}}>Renting</p>
+                </label>
+              </div>
+            </div>
+            <div style={{float: 'right', width: '50%'}}>
+              <div className="radio radio-primary">
+                <label>
+                  <input type='radio'/>
+                  <p style={{marginLeft: '-7px', marginTop: '-4px'}}>Buying</p>
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style={{clear: 'both'}}></div>
         <div className='col-md-6'>
           <h2>About my Suburb</h2>
           <SuburbPicker transitionCb={this.props.transitionCb} />
